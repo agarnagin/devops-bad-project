c331a4c385ce34949418de3812a38d7ec6529fec
agarnagin <agarnagin@exadel.com>
2024-11-05 11:45:01 +0200

    PR-1: initial commit

---
+++ src/test/java/org/exadel/bootcamp/devopsbadproject/DevopsBadProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package org.exadel.bootcamp.devopsbadproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsBadProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+        // empty
+	}
+
+}
